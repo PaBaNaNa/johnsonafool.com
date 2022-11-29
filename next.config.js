65a40929f76ad5920f34fada09b754c02a8e6a5e
johnsonafool <johnsonafool@gmail.com>
2022-11-29 17:49:29 +0800

    test: debug gh page deploy error

--- next.config.js
+++ next.config.js
@@ -13,11 +13,9 @@ const nextConfig = {
   // i18n,
 
   // Uncoment to add domain whitelist
-  // images: {
-  //   domains: [
-  //     'res.cloudinary.com',
-  //   ],
-  // },
+  images: {
+    domains: ['res.cloudinary.com'],
+  },
 
   // SVGR
   webpack(config) {
