79eaee156d831bdbb5c63096a55241c25f96db0b
johnsonafool <johnsonafool@gmail.com>
2022-12-05 02:18:44 +0800

    ci: gh page production test

--- next.config.js
+++ next.config.js
@@ -1,40 +1,7 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-// const { i18n } = require('./next-i18next.config');
-
-/** @type {import('next').NextConfig} */
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    dirs: ['src'],
-  },
-
   reactStrictMode: true,
   swcMinify: true,
-  // i18n,
-
-  // Uncoment to add domain whitelist
-  images: {
-    domains: ['res.cloudinary.com'],
-  },
-
-  // SVGR
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: [
-        {
-          loader: '@svgr/webpack',
-          options: {
-            typescript: true,
-            icon: true,
-          },
-        },
-      ],
-    });
-
-    return config;
-  },
 };
 
 module.exports = nextConfig;
